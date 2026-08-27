f2c0faac17677f7ae52f065fef90c52e71794219
Thomas <146830851+ThomasYang0318@users.noreply.github.com>
2026-08-27 10:37:57 +0800

    Bust portfolio asset caches

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-import { projectLookup, projectTracks } from './data/projects.js';
-import { skillGroups } from './data/skills.js';
-import { initCarousels } from './components/carousel.js';
-import { renderFeatureTours } from './components/feature-tour.js';
-import { renderGenericProjectDetail } from './components/project-detail.js';
-import { renderProjectGalleries, renderProjectTracks } from './components/projects.js';
-import { initNavigation, initRevealAnimations, initThemeToggle, restoreHashTarget, updateCopyrightYear } from './components/site.js';
-import { initSkillProjectChooser, renderSkillGroups } from './components/skills.js';
-import { renderProjectVideos, renderVideoEmbeds } from './components/videos.js';
+import { projectLookup, projectTracks } from './data/projects.js?v=20260827-2';
+import { skillGroups } from './data/skills.js?v=20260827-2';
+import { initCarousels } from './components/carousel.js?v=20260827-2';
+import { renderFeatureTours } from './components/feature-tour.js?v=20260827-2';
+import { renderGenericProjectDetail } from './components/project-detail.js?v=20260827-2';
+import { renderProjectGalleries, renderProjectTracks } from './components/projects.js?v=20260827-2';
+import { initNavigation, initRevealAnimations, initThemeToggle, restoreHashTarget, updateCopyrightYear } from './components/site.js?v=20260827-2';
+import { initSkillProjectChooser, renderSkillGroups } from './components/skills.js?v=20260827-2';
+import { renderProjectVideos, renderVideoEmbeds } from './components/videos.js?v=20260827-2';
 
 const pathPrefix = document.body.classList.contains('project-page') ? '../' : '';
 
